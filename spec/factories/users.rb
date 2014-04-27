feee8eba3d4d1de1eac707be402c21dbcce8b137
Mark Lehman <markopolo@gmail.com>
2014-04-27 14:54:44 -0700

    add unit testing for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
+    name Faker::Name.name
+    email Faker::Internet.email
+    username  Faker::Internet.user_name
+    password "password123"
+    password_confirmation "password123"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should validate_presence_of :password }
+  it { should validate_presence_of :username }
+  it { should validate_uniqueness_of :username }
+  it { should have_many :reports }
+
+  describe "full_size_pic" do
+    before do
+      @user = FactoryGirl.create :user
+    end
+    it "takes a photo url from Google+ and slices off the end to make it full-sized" do
+      @user.photo_url = "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50"
+      @user.full_size_pic.should eq "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg"
+    end
+    it "does not alter other photo urls" do
+      @user.photo_url = "www.example.com/photo.jpg"
+      @user.full_size_pic.should eq "www.example.com/photo.jpg"
+    end
+  end
 end
