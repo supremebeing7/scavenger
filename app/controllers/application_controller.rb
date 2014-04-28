bdb8ea3972ccbcdac8c7e675794041fa168be9e4
Mark Lehman <markopolo@gmail.com>
2014-04-27 20:10:14 -0700

    fix normal signup without Google

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :username) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :username, :email, :password) }
   end
 end
