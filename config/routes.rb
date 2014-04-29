c14162576a127f0f3420ff218179989168953362
Mark Lehman <markopolo@gmail.com>
2014-04-28 20:00:59 -0700

    link crawls, places and home page, and add place routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Scavenger::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :pages
   resources :crawls
+  resources :places
   root to: "pages#home"
 end
