c7f7d64864dce769202b9602e25b8e57da5926d5
supremebeing7 <student@epicodus.com>
2014-04-30 10:30:28 -0700

    seed database with user, crawl, and places with correct associations

--- spec/factories/crawls.rb
+++ spec/factories/crawls.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :crawl do
-    name "New Pub Crawl"
-    description "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet felis vel mauris sollicitudin adipiscing eget a massa. Vestibulum posuere, purus sed venenatis blandit, lectus mauris venenatis velit, sed vestibulum sapien risus non urna. Sed turpis eros, convallis non facilisis sed, fermentum in est. Integer id dapibus augue. Sed convallis, enim ac tincidunt aliquam, sapien metus blandit erat, sit amet ullamcorper massa eros suscipit felis. Curabitur egestas, arcu facilisis dignissim imperdiet, libero tortor commodo lorem, vitae vestibulum turpis risus ac nulla. Nulla rhoncus elit sed risus accumsan dictum. Aliquam vitae velit vehicula, pulvinar elit eget, sagittis diam. Integer ac diam felis."
+    name "Steve is 30!"
+    description "Celebrating Steve's 30th birthday with a wicked scavenger hunt all over Portland!"
   end
 end
