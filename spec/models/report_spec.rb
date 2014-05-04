99a8b4179b7e5956323fbc84650d4715771c45ea
Mark Lehman <markopolo@gmail.com>
2014-05-04 15:07:29 -0700

    fix tests so all are passing

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -6,13 +6,24 @@ describe Report do
   it { should belong_to :user }
   it { should belong_to :crawl }
 
-  it "sends an email to admin and user when the user reports content" do
+  it "sends an email to admin and user when the user reports content for place" do
     user = FactoryGirl.create :user
     place = FactoryGirl.create :place
     report = FactoryGirl.build :report
     report.update(user_id: user.id, place_id: place.id)
     report.save
-    ActionMailer::Base.deliveries[-2].to.should eq ["admin@hoursofhappiness.com"]
+    ActionMailer::Base.deliveries[-2].to.should eq ["markopolo@gmail.com"]
+    ActionMailer::Base.deliveries.last.to.should eq [report.user.email]
+  end
+  it "sends an email to admin and user when the user reports content for crawl" do
+    user = FactoryGirl.create :user
+    crawl = FactoryGirl.build :crawl
+    crawl.update(user_id: user.id)
+    crawl.save
+    report = FactoryGirl.build :report
+    report.update(user_id: user.id, crawl_id: crawl.id)
+    report.save
+    ActionMailer::Base.deliveries[-2].to.should eq ["markopolo@gmail.com"]
     ActionMailer::Base.deliveries.last.to.should eq [report.user.email]
   end
 end
