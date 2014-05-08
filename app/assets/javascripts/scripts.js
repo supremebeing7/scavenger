155cd8f33e365069123e0cb25ec9ce5e0d313612
Mark <student@epicodus.com>
2014-05-08 14:33:21 -0700

    get generic messaging works, still need to config for specific hunts and users

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,7 @@
 $(function() {
   var faye = new Faye.Client('http://localhost:9292/faye');
+  alert('subscribing...');
   faye.subscribe('/messages', function (data) {
-    alert(data);
+    eval(data);
   })
 });
