5404fc2362b97aea079f090133f6286684f4618b
Mark Lehman <markopolo@gmail.com>
2014-11-06 17:14:12 -0800

    refactors feedback spec

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe Feedback do
+  before do
+    Feedback.create(email: "satisfied@user.com", message: "Site looks swell!")
+  end
+
   it "sends an email to admin when someone submits feedback" do
-    @submission = Feedback.create(email: "satisfied@user.com", message: "Site looks swell!")
     ActionMailer::Base.deliveries.last.subject.should eq 'New Feedback!'
   end
   it "has a reply-to address for the person submitting feedback" do
-    @submission = Feedback.create(email: "satisfied@user.com", message: "Site looks swell!")
     ActionMailer::Base.deliveries.last.reply_to.should eq ['satisfied@user.com']
   end
 end
